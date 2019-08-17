4f0a55657fd897f0f273348ab81529d0b73ecb56
hyansmn <hyankelwin@hotmail.com>
2019-08-17 12:33:55 -0300

    JavaSpring

---
+++ JavaSpring/ProjetoSpring/src/main/java/com/spring/api/ProjetoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+		System.out.println("Esta é uma alteração no projeto");
+	}
+
+}
